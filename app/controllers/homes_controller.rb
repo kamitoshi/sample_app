e50752a45633d2e4eea9b4fa3e7a78e1b9f0552d
kamitoshi <kroo.aq.74@gmail.com>
2020-03-16 07:33:29 +0000

    [finish] 3章

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,5 @@
 class HomesController < ApplicationController
   def top
-    
+    @list = List.all
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '23f15f0a0b17aa9b1ce3ee68b9ce599def7503bf336e3cd5b6c1e0458947fa4d59d49306a476d445e8cd4afbb01637a6b887c71ffdd8aa56d1134fb3bab56c33'
